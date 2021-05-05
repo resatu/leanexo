1f5c29fe4544b13aa0ad1f3ec87ef2522c41ba5e
resatugurulu <resatugur.ulu@gmail.com>
2021-05-05 21:56:47 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+    var myArr = [1, 2, 3, 4];
+    return (
+        <div>
+            {
+                myArr.map(i => {
+                    return <h1 key={i} className="text-5xl text-red-800" >About Page</h1>
+                }
+                )
+            }
+        </div>
+    )
+}
+export default About
+
+
+
+// const a = [a, b, c, d, e, f]
+// return ({
+//     a.forEach(i => { <h1>About Page</h1> })
+// })
