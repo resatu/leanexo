38d71d38a7f37b0737cbd3ccce830bbdd7c3b20c
resatugurulu <resatugur.ulu@gmail.com>
2021-05-11 13:43:05 +0300

    removed homepage & updated the documentation

--- pages/index.js
+++ pages/index.js
@@ -4,67 +4,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>LeanExo - Build your exponential canvas online </title>
-        <meta name="description" content="Transforming the world for a better future" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      {/* Header */}
+      <div>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to LeanExo
-        </h1>
-
-        <p className={styles.description}>
-          Lead yourself   {' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>References &rarr;</h2>
-            <p>Discover and learn which businesses worked with us.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Contact &rarr;</h2>
-            <p>
-             Reach out to us any time, we are looking forward to hearing from you
-
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      </div>
+      {/* Footer */}
+      <div></div>
     </div>
   )
 }
