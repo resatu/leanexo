949f51af47baed315ec1ccbbfc4c35b306859bf2
resatugurulu <resatugur.ulu@gmail.com>
2021-05-22 22:22:10 +0300

    navbar changes

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,32 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      {/* Header */}
-      <div>
-
+      {/* Navbar */}
+      <div className="justify-between">
+        <nav className="bg-white inline-flex items-center">
+          {/* Logo */}
+          <div className="flex-1 container">
+            <a href="#">
+              <img
+                className="w-10 h-10"
+                src="http://static1.squarespace.com/static/5b33ad559772aedf7cd44bf7/t/5b3fed222b6a28fc88a422f2/1530916131998/ms-icon-310x310.png?format=2w">
+              </img>
+            </a>
+          </div>
+          <div>
+            <div className="flex-1">
+              <a href="https://www.google.com">Lean Exo</a>
+            </div>
+          </div>
+        </nav>
+        {/* Sign In */}
+        <div>
+          <a
+            className="border-4 "
+            href="#">Sign in with Google</a>
+        </div>
       </div>
-      {/* Footer */}
+      {/* Hero */}
       <div></div>
     </div>
   )
