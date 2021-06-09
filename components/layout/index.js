28e46b18f241dcf091b527afa396ecbc935ed613
resatugurulu <resatugur.ulu@gmail.com>
2021-06-10 02:52:56 +0300

    favicon added

--- components/layout/index.js
+++ components/layout/index.js
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
                 <title>
                     Lean ExO
                 </title>
-                <link rel="shortcut icon" type="image/x-icon" href="https://images.squarespace-cdn.com/content/v1/5b33ad559772aedf7cd44bf7/1530916305138-JT9LH8WZLC3WRPCMATLR/ke17ZwdGBToddI8pDm48kOmh8RaYNLpLTpNRk-8zo6VZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVHdiPr3J1iMUIEhBI-aakGsSirXgBxrEvtyiouW2tF3HG0nsU3dfn6w--du8-EjPUE/favicon.ico?format=100w" />
+                <link rel="shortcut icon" href="/images/favicon/favicon.ico" />
             </Head>
             <Navigation />
             <main>{children}
